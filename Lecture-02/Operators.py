e4996d9042db82c79380bd9e27474abd3555ad41
tanapoom302548 <tangg25488za@gmail.com>
2026-07-07 15:30:06 +0700

    Lecture-02

---
+++ Lecture-02/Operators.py
@@ -0,0 +1,25 @@
+x = 15
+y = 4
+
+# Output : x + y = 19
+print ('x + y =',x+y)
+
+# Output : x - y = 11
+print ('x - y =',x-y)
+
+# Output : x * y = 60
+print ('x * y =',x*y)
+
+# Output : x / y = 3.75
+print ('x / y =',x/y)
+
+# Output : x % y = 3
+print ('x % y =',x%y)
+
+# Output : x // y = 3
+print ('x // y =',x//y)
+
+# Output : x ** y = 50625
+print ('x ** y =',x**y)
+
+print(5+18//2-25%4)
